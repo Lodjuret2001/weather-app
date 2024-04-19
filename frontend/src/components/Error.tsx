a57f59a12301d9fa936f2b1362c80ca726c9ba09
Pontus Norup <minecraftpontus@gmail.com>
2024-04-19 11:58:06 +0200

    Fixed error text & bug

--- frontend/src/components/Error.tsx
+++ frontend/src/components/Error.tsx
@@ -8,23 +8,12 @@ type Props = {
 const Error = ({ error }: Props) => {
   const buttonCSS: string =
     "border-2 border-blue-200 ml-5 bg-blue-200 p-[6px] rounded-md hover:bg-transparent hover:border-orange-300 hover:text-white";
-
+  const font: string =
+    "font-['cloudy'] text-center mt-12 mb-12 text-white text-[1rem] sm:text-2xl w-4/5";
   return (
     <div className="flex flex-col items-center justify-center">
       <img className="w-2/5" src={errorCloudImage} />
-      <p
-        style={{
-          fontFamily: "cloudy",
-          textAlign: "center",
-          marginTop: "30px",
-          marginBottom: "30px",
-          color: "white",
-          fontSize: "2rem",
-          width: "80%",
-        }}
-      >
-        {error}
-      </p>
+      <p className={font}>{error}</p>
       <button onClick={() => window.location.reload()} className={buttonCSS}>
         Try again / Return home
       </button>
